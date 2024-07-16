41914814dd26a0d67e5aff16bf6a1663111c3391
Narek <ngrigoryan228@gmail.com>
2024-07-17 00:13:54 +0400

    finish project

--- script-home.js
+++ script-home.js
@@ -8,9 +8,10 @@ const indexPath = "./index.html";
 const baseURL = "https://bever-aca-assignment.azurewebsites.net/users";
 const InvoiceUrl = "https://bever-aca-assignment.azurewebsites.net/invoices";
 const InvoiceLinesUrl ="https://bever-aca-assignment.azurewebsites.net/invoicelines";
-const ProductUrl="https://bever-aca-assignment.azurewebsites.net/products"
+const ProductUrl = "https://bever-aca-assignment.azurewebsites.net/products";
 let someData = [];
 isLogged_userName.innerText = userName;
+
 
 window.onload = function () {
   const tokenId = localStorage.getItem("token");
@@ -41,32 +42,46 @@ const getData = (event) => {
       fetch(InvoiceLinesUrl)
         .then((resp) => resp.json())
         .then((data) => {
+          let totalAmount = 0;
+          let mybigData;
           let filtered = data.value.filter((elem) => elem.InvoiceId === myId);
-          console.log(filtered, someData);
           service_lines_body.innerHTML = "";
           someData.length &&
             someData.forEach((el) => {
               let myData = filtered.filter(
                 (elem) => el.ProductId === elem.ProductId
               );
-            
-
+              mybigData = myData;
+              totalAmount += el.Price * myData[0].Quantity;
               let row = document.createElement("tr");
               row.innerHTML = `
-          <td> ${el.Name} </td>
-          <td> ${el.Price} </td>
-          <td> ${myData[0].Quantity} </td>
-          <td> ${el.Price * myData[0].Quantity}</td>
-  
-        `;
+              <td> ${el.Name} </td>
+              <td> ${el.Price} </td>
+              <td> ${myData[0].Quantity} </td>
+              <td > ${el.Price * myData[0].Quantity}</td>
+              `;
               service_lines_body.append(row);
             });
+          const invoiceId = mybigData[0].InvoiceId;
+          const totalTabelCell = document.getElementById(invoiceId);
+          totalTabelCell.innerHTML = totalAmount;
         });
     });
 };
+//
+///
+///
+////Invoice-i Total amount-y zut stacelem  Invoiclines Total amount gumaric
 
+///
+////
+////
+////InvoiceLines Total amount-y stacelem Price*Quantity
+//
+///
+///
+//
 const printInvoices = () => {
-  const data = [];
   fetch(InvoiceUrl)
     .then((resp) => resp.json())
     .then(({ value }) => {
@@ -74,17 +89,14 @@ const printInvoices = () => {
         value.map((el) => {
           if (el.UserId === localStorage.getItem("token")) {
             const row = document.createElement("tr");
-
             row.innerHTML = `
             <td>
             <input type="radio" name="clickedInput" value="${el.Name}">
             </td>
             <td>${el.Name}</td>
             <td>${el.PaidDate}</td>
-             <td>${el.InvoiceId}</td>
-            <td></td>
+            <td id=${el.InvoiceId}>--</td>
           `;
-
             TableBody.appendChild(row);
           }
         });
@@ -107,4 +119,3 @@ LogOutBtn.addEventListener("click", () => {
   localStorage.removeItem("token");
   window.location.replace(indexPath);
 });
-
--- script.js
+++ script.js
@@ -1,218 +1,66 @@
-// const button = document.querySelector("button");
-// const axios = required("axios");
-// const name = document.getElementById("name");
-// const password = document.getElementById("password");
-// const loginform = document.querySelector(".loginform");
-// const userprofile = document.querySelector(".user-profile");
+const baseURL = "https://bever-aca-assignment.azurewebsites.net/users";
+const homePath = './home.html'
+const tokenId = localStorage.getItem("token");
 
-//   const request=fetch("https://bever-aca-assignment.azurewebsites.net/users")
-//   const resp=request.then(response=>{
-//     if(!response.ok){
-//       throw Error("Errorr")
-//   }
-//     return response.json()
-//   })
+const onSubmit = () => {
+  const userName = document.getElementById("name").value;
+  const password = document.getElementById("password").value;
+  localStorage.setItem('userName', userName);
 
-//   const data=resp.then(data=>{
-//    console.log(data.value)
-//    return data
+  const dataToSend = {
+    name: userName,
+    password,
+  };
 
-//   })
-//   .catch(error=>{
-//     printError
-//   })
+  fetch(baseURL)
+    .then(resp => resp.json())
+    .then(({ value }) => {
+      if (value) {
+        const usersMap = new Map(value.map((user) => [user.Name, user]));
+        const usersMapToken = new Map(value.map((user) => [user.UserId, user]));
+        if (usersMapToken.has(tokenId)) {
+          window.location.replace(homePath);
+        }
+        if (
+          (usersMap.get(dataToSend.name) &&
+            usersMap.get(dataToSend.name).Password === dataToSend.password) ||
+          localStorage.getItem("token")
+        ) {
 
-// console.log(data)
+          localStorage.setItem("token", usersMap.get(dataToSend.name).UserId)
+          window.location.replace("home.html");
+          if (localStorage.getItem("token")) {
+            window.location.replace("home.html");
+          }
+        } else if (
+          usersMap.get(dataToSend.name) !== undefined &&
+          !usersMap.get(dataToSend.name).password
+        ) {
+          alert("Password wrong");
+          localStorage.clear("token");
+        } else if (!usersMap.has(dataToSend.name)) {
+          alert("Login and Password wrong");
+          localStorage.clear("token");
+        }
+      }
+    });
+};
 
-// const form = {
-//   name: document.getElementById("name"),
-//   password: document.getElementById("password"),
-//   button: document.querySelector(".btn"),
-//   messages: document.getElementById(".message"),
-// };
 
-// const user = {
-//   name: name.value,
-//   password: password.value,
-// };
-
-// const onSubmit = form.button.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   console.log(form);
-//   const login = "https://bever-aca-assignment.azurewebsites.net/users";
-//   localStorage.setItem("user", JSON.stringify(user));
-//   console.log("asdas");
-//   name.value = "";
-//   password.value = "";
-
-//   fetch(login, {
-//     method: "POST",
-//     headers: {
-//       Accept: "application/json, text/plain, */*",
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       name: form.name.value,
-//       password: form.password.value,
-//     }),
-//   })
-//     .then((response) => response.json())
-//     .then((data) => {
-//       console.log(data.value);
-//       // code here //
-//       if (data.error) {
-//         alert("Error Password or Username"); /*displays error message*/
-//       } else {
-//         window.open(
-//           "target.html"
-//         ); /*opens the target page while Id & password matches*/
-//       }
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-
-  // fetch("https://bever-aca-assignment.azurewebsites.net/users").then((resp) => {
-  //   return resp
-  //     .json()
-  //     .then((data) => {
-  //       data.value.forEach((el) => {
-  //         console.log(el);
-  //         if (el.Name === data1.name && el.Password === data1.password) {
-  //           alert("Error Password or Username"); /*displays error message*/
-  //           console.log(el.Name);
-  //         } else if (data.error) {
-  //           window.open(
-  //             "target.html"
-  //           ); /*opens the target page while Id & password matches*/
-  //         }
-  //       });
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // });
-// });
-
-// fetch(login, {
-//       method: "POST",
-//       headers: {
-//         Accept: "application/json, text/plain, */*",
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         name: form.name.value,
-//         password: form.password.value,
-//       }),
-//     })
-//       .then((response) => response.json())
-//       .then((data) => {
-//         console.log(data.value);
-//         // code here //
-//         if (data.error) {
-//           alert("Error Password or Username"); /*displays error message*/
-//         } else {
-//           window.open(
-//             "target.html"
-//           ); /*opens the target page while Id & password matches*/
-//         }
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-
-// let data = fetch("https://bever-aca-assignment.azurewebsites.net/users")
-//     .then((response) => response.json())
-//     .then(data => {
-//         // do some stuff
-//         console.log(data.value)
-//         return data;
-//     })
-//     .catch(error => {
-//         return error;
-//     });
-
-// const printUserProfile= () => {
-//   const user=JSON.parse(localStorage.getItem("user"));
-//   if(user){
-//     loginform.style.display="none"
-//     userprofile.style.display="block"
-
-//     const ul=document.createElement("ul")
-//     const liName=document.createElement("li")
-//     const liPassword=document.createElement("li")
-
-//     liName.textContent=`Name: ${user.name}`
-//     liPassword.textContent=`Password: ${user.password}`
-
-//     ul.appendChild(liName)
-//     ul.appendChild(liPassword)
-//     userprofile.appendChild(ul)
-
-//   }
-
-// }
-
-// button.addEventListener("click", () => {
-//   const user = {
-//     name: name.value,
-//     password: password.value,
-//   };
-//   localStorage.setItem("user", JSON.stringify(user));
-//   name.value = "";
-//   password.value = "";
-//   printUserProfile();
-// });
-
-// window.addEventListener("load", printUserProfile);
-
-// const printError=(error)=>{
-//   const errorElement=document.createElement("div")
-//   errorElement.textContent=error.message
-//   container.appendChild(errorElement)
-// }
-// console.log()
-
-// const apiBaseUrl="https://bever-aca-assignment.azurewebsites.net/invoices"
-
-// const data2=fetch(`${apiBaseUrl}`)
-// const items=data2.then((resp)=>{
-//   return resp.json()
-// })
-// const newItem=items.then((res)=>{
-//   return res.value
-// })
-
-// loginform.appendChild(newItem)
-// const container=document.getElementById("container")
-
-// const printInvoices=(items)=>{
-//     items.forEach(element => {
-//         const InvoicesName=document.createElement("div")
-//         InvoicesName.textContent=element.Name
-//         const InvoicesData=document.createElement("div")
-//         InvoicesData.textContent=element.PaidDate
-//         container.appendChild(InvoicesName)
-//         container.appendChild(InvoicesData)
-//     });
-
-// }
-
-// const fetchData=()=>{
-//         fetch("https://bever-aca-assignment.azurewebsites.net/invoices")
-//         .then(response=>{
-//             if(!response.ok){
-//                 throw Error("Errorr")
-//             }
-//             return response.json()
-//         })
-//         .then(data=>{
-//           printInvoices(data.value)
-//           console.log(data.value)
-//         })
-//         .catch(error=>{
-//           printError
-//         })
-// }
-
-// fetchData()
+window.onload = function () {
+  const tokenId = localStorage.getItem("token");
+  fetch(baseURL, {
+    method: "GET",
+  })
+    .then((resp) => {
+      return resp.json();
+    })
+    .then(({ value }) => {
+      if (value) {
+        const usersMap = new Map(value.map((user) => [user.UserId, user]));
+        if (usersMap.has(tokenId)) {
+          window.location.replace(homePath)
+        }
+      }
+    });
+};
